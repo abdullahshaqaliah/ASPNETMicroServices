91a5edcb30da3fc1e9109b8244691342e311f200
Abdullah Shaqalaih <abdullah@trustangle.com>
2021-10-23 21:01:31 +0300

    ProductRepository Is Created

---
+++ src/Services/Catalog/Catalog.API/Data/Interface/ICatalogContext.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.API.Entitites;
+using MongoDB.Driver;
+namespace Catalog.API.Data.Interface
+{
+    public  interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
